6a132fe9f6e40a0ae0a7156a230218903d218ffc
YersonCC <y.cenon@udla.edu.co>
2025-10-18 19:05:10 -0500

    feat: initialize bio-portales project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold">
+        Hello, Biodiversity Portals!
+      </h1>
+    </div>
+  )
+}
